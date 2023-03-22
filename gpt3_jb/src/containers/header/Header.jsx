89b0ec39bfd22b105391bc281429318ce5288230
Euroshift <50931771+Euroshift@users.noreply.github.com>
2023-03-22 15:35:00 -0700

    React Application-GPT3

---
+++ gpt3_jb/src/containers/header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+
+const Header = () => {
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Test copy test copy test copy
+        </h1>
+        <p>
+        Copy Copy Copy Copy Copy Copy description
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address"/>
+            <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>
+            1,600 people requested access - Copy copy
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
+      </div>
+    </div>
+  )
+}
+
+export default Header
